903e39b69f690612cf36b67d89f9380064b98dfe
Italo Costa <66748127+italomarcelogit@users.noreply.github.com>
2023-12-25 09:28:17 -0300

    Update loguru.py

--- loguru.py
+++ loguru.py
@@ -5,11 +5,12 @@ from datetime import datetime
 logger.add("meuapp.log", format="{extra[ip]} | {extra[agora]} ({extra[user]}): {message}",
            rotation="1 week", backtrace=True, diagnose=True) 
 
-logger.bind(user="mariajose").info("XPto:> usuário fez login com sucesso")
+agora = datetime.now().strftime("%Y-%d-%m %H:%M:%S")
+logger.bind(ip="192.168.50.1", agora=agora, user="mariajose").info("XPto:> usuário fez login com sucesso")
 
 task_id=1
 with logger.contextualize(task=task_id):
     calculo = 1 + 2
-    logger.bind(user="mariajose").info(f"fim da tarefa {task_id}")
+    logger.bind(ip="192.168.50.1", agora=agora,user="mariajose").info(f"fim da tarefa {task_id}")
 
-logger.bind(user="api_bancoXP").warning("Acesso não autorizado")
+logger.bind(ip="192.168.50.1", agora=agora, user="api_bancoXP").warning("Acesso não autorizado")
